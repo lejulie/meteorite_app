ce87c4a68622a28df333c3348804f95fbad5f0be
lejulie <julieblairlevine@gmail.com>
2018-07-20 12:18:48 -0400

    add copy to welcome page

--- global.R
+++ global.R
@@ -9,32 +9,32 @@ library(DT)
 library(dplyr)
 
 # Read the data
-meteorites = read.csv("./data/meteorite_landings.csv")
+meteorites = read.csv("./data/cleaned_meteorites.csv")
 #backup = meteorites
 #meteorites = backup
 
 #####   DATA CLEANING - TO DO EXTERNALLY #####
 
-# Update some column names
-names(meteorites)[names(meteorites) == 'reclat'] <- 'lat'
-names(meteorites)[names(meteorites) == 'reclong'] <- 'long'
-names(meteorites)[names(meteorites) == 'recclass'] <- 'class'
-names(meteorites)[names(meteorites) == 'mass..g.'] <- 'mass'
-
-# Convert mass and year to numerics
-meteorites$mass = as.numeric(meteorites$mass)
-meteorites$year = as.numeric(substr(meteorites$year, 7,10))
-
-# Remove any nonsense points and unnecessary columns
-meteorites = filter(meteorites, year < 2018)
-meteorites = filter(meteorites, long > -180 & long < 180)
-meteorites = filter(meteorites, lat > -90 & lat < 90)
-meteorites = mutate(meteorites, keep = ifelse(lat ==0 & long ==0, "drop", "keep"))
-meteorites = filter(meteorites, keep == "keep")
-meteorites = select(meteorites, -GeoLocation, -keep)
-
-# remove points missing lat/lng
-meteorites = meteorites[complete.cases(meteorites),]
+# # Update some column names
+# names(meteorites)[names(meteorites) == 'reclat'] <- 'lat'
+# names(meteorites)[names(meteorites) == 'reclong'] <- 'long'
+# names(meteorites)[names(meteorites) == 'recclass'] <- 'class'
+# names(meteorites)[names(meteorites) == 'mass..g.'] <- 'mass'
+# 
+# # Convert mass and year to numerics
+# meteorites$mass = as.numeric(meteorites$mass)
+# meteorites$year = as.numeric(substr(meteorites$year, 7,10))
+# 
+# # Remove any nonsense points and unnecessary columns
+# meteorites = filter(meteorites, year < 2018)
+# meteorites = filter(meteorites, long > -180 & long < 180)
+# meteorites = filter(meteorites, lat > -90 & lat < 90)
+# meteorites = mutate(meteorites, keep = ifelse(lat ==0 & long ==0, "drop", "keep"))
+# meteorites = filter(meteorites, keep == "keep")
+# meteorites = select(meteorites, -GeoLocation, -keep)
+# 
+# # remove points missing lat/lng
+# meteorites = meteorites[complete.cases(meteorites),]
 
 ##### GLOBAL VARS #####
 
--- ui.R
+++ ui.R
@@ -8,11 +8,98 @@ ui <- fluidPage(
     
     ##### Welcome Page #####
     tabPanel("Welcome!",
+      # H1
       tags$div(class = "header", checked = NA,
-               tags$h1("Let's Talk About Meteorites"),
-               tags$p("Placeholder copy here")
-      )
-    ),
+       tags$h1("Let's Talk About Meteorites")),
+      
+      # What is a meteorite
+      tags$div(class = "body", checked = NA,
+       tags$h2('What is a meteorite?'),
+       tags$p('According to ',
+         tags$a(href=
+'https://www.nasa.gov/audience/forstudents/k-4/dictionary/Meteorite.html',
+          'NASA'),', a meteorite is “a rock 
+          that has fallen to Earth from outer space.”  When a piece of debris 
+          from an object (such as a comet or asteroid) enters the atmosphere of 
+          a planet or moon (such as Earth), it becomes a ',tags$em('meteor'),
+          ' as it descends towards the surface.  If the meteor survives passage 
+          all the way to the planet or moon’s surface, it is then considered a ',
+          tags$em('meteorite'),'.'
+       ), # close paragraph
+      tags$p('Meteors vary greatly in size, shape, and composition.  This app 
+          lets you explores some of these attributes for over 30,000 meteorites 
+          here on Earth.'), #close paragraph
+      
+      # What can you learn in this app?
+      tags$h2('What can you learn in this app?'),
+      tags$p('This app uses ',tags$a(href=
+          'https://data.nasa.gov/Space-Science/Meteorite-Landings/gh4g-9sfh',
+          'this data'),' aggregated by ',tags$a(href=
+          'http://meteoriticalsociety.org/','The Meteoritical Society'),
+          ' including information on the characteristics below:'),
+        
+        tags$h3('Meteorite sites'),
+        tags$p('Check out a map of where meterorites where found across the globe.'),
+
+        tags$h3('Fell vs. found'),
+        tags$p('Meterorites can be classified on whether or not a person saw their 
+               descent to Earth.  A meteorite that someone witnessed falling to Earth 
+             and then successfully tracked down is classified as a ',tags$em('fall'),
+               'or ',tags$em('fell'),'.  One that was determined to be a meteorite by 
+               examination of its properties is classified as a ',tags$em('found'),' 
+               meteorite.  See ',tags$a(href=
+               'https://en.wikipedia.org/wiki/Meteorite_fall','Wikipedia'),' for 
+               more.'),
+
+        tags$h3('Meteorite categorization'),
+        tags$p('Meteorites are also classified by their physical, chemical, isotopic, 
+               and mineralogical properties, with the goal of grouping them ultimately 
+               by their origin (rocks from the same source should be made of similar 
+               stuff).  There are several taxonomies for classifying meterorites.  
+               According to ',tags$a(href=
+               'https://www.lpi.usra.edu/meteor/notes.php?note=6','the Meteoritical 
+               Society'),'website,'),
+        tags$blockquote('If the meteorite was published in both the Catalogue of 
+                        Meteorites and MetBase (see columns NHMCat and MetBase), both 
+                        classifications will appear if they do not agree. If the 
+                        meteorite was just published in one of these sources, the 
+                        classification from that source will be listed. If the 
+                        meteorite was published in neither, the classification comes 
+                        from the Meteoritical Bulletin (approved names) or from 
+                        unreviewed reports (provisional names).'),
+         tags$p('You can read more about the classification of meteorites on ',
+         tags$a(href='https://en.wikipedia.org/wiki/Meteorite_classification',
+                'Wikipedia.')),
+         tags$h3('Meteorite mass'),
+         tags$p('Meteorite masses in this dataset are in grams.  A note on masses 
+              from ,',tags$a(href='https://www.lpi.usra.edu/meteor/notes.php?note=16',
+                               'the Meteoritical Society'),' website.'),
+
+         # Where does this data come from?
+         tags$h2('Where does the data come from?'),
+         tags$p('This data is provided by the ',tags$a(href='https://data.nasa.gov/',
+                'NASA Open Data Portal.'),'  The source can be found  ',tags$a(href=
+                'https://data.nasa.gov/Space-Science/Meteorite-Landings/gh4g-9sfh',
+                'here.'),'  The data is originated from ',tags$a(href=
+                'http://meteoriticalsociety.org/','The Meteoritical Society.')),
+          tags$p('This app was built using the ',tags$a(href=
+                'https://shiny.rstudio.com/','Shiny package'),' for R.  Check out the 
+                code on ',tags$a(href='https://github.com/lejulie/','github.')),
+
+         # Who are you?
+         tags$h2('Who are you?'),
+         tags$p('My name is Julie Levine.  I’m a graduate from the School of 
+                Engineering and Applied Science at the University of Pennsylvania.  
+                In a past life I was a marketer and product manager for tech 
+                startups ',tags$a(href='https://www.factual.com/','Fatual'),' and ',
+         tags$a(href='https://www.datadoghq.com/','Datadog.'),'Presently, I’m a Data 
+         Science Fellow at ', tags$a(href='https://nycdatascience.com/','NYC Data 
+         Science Academy.'),'Check out more of my projects on the ',tags$a(href=
+         'https://nycdatascience.com/blog/author/lejulie/','NYC Data Science Academy 
+         blog.'))
+
+      ) # close div
+    ), # end welcome tab
     
     ##### Map ##### 
     tabPanel("Map Meteorites", 
